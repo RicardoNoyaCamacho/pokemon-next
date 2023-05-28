79a1fc04a7bfc573e26ef844eff3f15d8531546c
RicardoNoyaCamacho <83376211+RicardoNoyaCamacho@users.noreply.github.com>
2023-05-28 13:35:58 -0600

    Update NoFavorites.tsx

--- components/ui/NoFavorites.tsx
+++ components/ui/NoFavorites.tsx
@@ -1,30 +1,27 @@
 import { Container, Image, Text } from "@nextui-org/react";
 
-import { Layout } from "../layouts";
-
 export const NoFavorites = () => {
   return (
-    <Layout title="Pokemon - Favoritos">
-      <Container
+    <Container
+      css={{
+        display: "flex",
+        flexDirection: "column",
+        height: "calc(100vh - 100px)",
+        alignItems: "center",
+        justifyContent: "center",
+        alignSelf: "center",
+      }}
+    >
+      <Text h1> No hay Favoritos</Text>
+      <Image
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg"
+        alt=""
+        width={250}
+        height={250}
         css={{
-          display: "flex",
-          flexDirection: "column",
-          height: "calc(100vh - 100px)",
-          alignItems: "center",
-          justifyContent: "center",
-          alignSelf: "center",
+          opacity: 0.1,
         }}
-      >
-        <Text h1> No hay Favoritos</Text>
-        <Image
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg"
-          width={250}
-          height={250}
-          css={{
-            opacity: 0.1,
-          }}
-        />
-      </Container>
-    </Layout>
+      />
+    </Container>
   );
 };
